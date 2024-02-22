5a68e62a8a5f5a03a0960735ea1d33ed6d555da4
Zakócs János <j.zakocs03@gmail.com>
2024-02-22 11:15:06 +0100

    DataGridView megjelnítés

--- Fibonacci/Form1.cs
+++ Fibonacci/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace Fibonacci
 {
     public partial class Form1 : Form
@@ -5,29 +7,41 @@ namespace Fibonacci
         public Form1()
         {
             InitializeComponent();
-            for (int i = 0;i < 10; i++)
+            List<Elem> lista = new List<Elem>();
+            for (int i = 0; i < 10; i++)
             {
-                Button b = new Button();
+                /*Button b = new Button();
                 b.Top = i * 30;
                 b.Text = Fibszam(i).ToString();
-                Controls.Add(b);
+                Controls.Add(b);*/
+                Elem elem = new Elem();
+                elem.Sorszam = i;
+                elem.Ertek = Fibszam(i);
+                lista.Add(elem);
             }
+            dataGridView1.DataSource = lista;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private int Fibszam(int n)
+        public int Fibszam(int n)
         {
             if (n == 0)
             {
                 return 0;
             }
-            if (n == 1) {
+            if (n == 1)
+            {
                 return 1;
             }
-            return Fibszam(n - 1) + Fibszam(n-2);
+            return Fibszam(n - 1) + Fibszam(n - 2);
         }
+    }
+    class Elem
+    {
+        public int Sorszam { get; set; }
+        public long Ertek { get; set; }
     }
 }

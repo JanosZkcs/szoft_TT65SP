5442a62c8a60ce67d2442bd021e31def5d7f46e1
jancsi03 <j.zakocs02@gmail.com>
2024-02-29 10:13:26 +0100

    Változtatások

---
+++ gyakorlat_3/Program.cs
@@ -0,0 +1,35 @@
+namespace gyakorlat_3
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+    public class VillogoGomb : Button
+    {
+        public VillogoGomb()
+        {
+            MouseEnter += VillogoGomb_MouseEnter;
+            MouseLeave += VillogoGomb_MouseLeave;
+        }
+
+        private void VillogoGomb_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.ButtonFace;
+        }
+
+        private void VillogoGomb_MouseEnter(object sender, EventArgs e)
+        {
+            BackColor = Color.Yellow;
+        }
+    }
+}

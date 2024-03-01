f27b7051dd0bd89a4d4990b070b73c9ba465c321
Zakócs János <j.zakocs03@gmail.com>
2024-03-02 00:09:48 +0100

    Karácsonyfa

--- gyakorlas_zh/Form1.cs
+++ gyakorlas_zh/Form1.cs
@@ -1,4 +1,4 @@
-namespace gyakorlas_zh
+﻿namespace gyakorlas_zh
 {
     public partial class Form1 : Form
     {
@@ -9,6 +9,8 @@ namespace gyakorlas_zh
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            /*
+            //Gombok főátlós és alatta
             for(int sor = 0; sor < 10; sor++)
             {
                 for(int oszlop = 0; oszlop < 10; oszlop++)
@@ -23,6 +25,38 @@ namespace gyakorlas_zh
                         Controls.Add(gomb);
                     }
                 }
+            }*/
+            //6.feladat: karácsonyfa
+            int tolas = ClientRectangle.Width / 2;
+            for (int sor = 0; sor <= 11; sor++)
+            {
+                for (int oszlop = 0; oszlop <= 11; oszlop++)
+                {
+                    if (oszlop <= sor)
+                    {
+                        if (sor < 10)
+                        {
+                            Button gomb = new Button();
+                            gomb.BackColor = Color.Green;
+                            gomb.Top = sor * 20;
+                            gomb.Left = tolas + oszlop * 20;
+                            gomb.Width = 20;
+                            gomb.Height = 20;
+                            Controls.Add(gomb);
+                        }
+                        else
+                        {
+                            Button talp = new Button();
+                            talp.BackColor = Color.Brown;
+                            talp.Width = 20;
+                            talp.Height = 20;
+                            talp.Top = sor * 20;
+                            talp.Left = ClientRectangle.Width / 2;
+                            Controls.Add(talp);
+                        }
+                    }
+                }
+                tolas -= 10;
             }
         }
     }

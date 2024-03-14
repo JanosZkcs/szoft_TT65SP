fbb95c3fe1e8f25fd6d952ecf8308b579b58a20e
Zakócs János <j.zakocs03@gmail.com>
2024-03-14 16:33:52 +0100

    try catch beolvasáshoz

--- Hajos teszt/Form1.cs
+++ Hajos teszt/Form1.cs
@@ -25,28 +25,36 @@ namespace Hajos_teszt
         }
         List<Kerdes> KerdesBeolvasas()
         {
-            List<Kerdes> lista = new List<Kerdes>();
-            StreamReader sr = new StreamReader("C:\\Users\\jzako\\source\\repos\\szoft_TT65SP\\Hajos teszt\\text.txt", true);//Local
-            while(!sr.EndOfStream) { 
-                string sor = sr.ReadLine() ?? string.Empty;
-                string[] tomb = sor.Split('\t');
-                if (tomb.Length != 7) continue;
+            try
+            {
+                List<Kerdes> lista = new List<Kerdes>();
+                StreamReader sr = new StreamReader("C:\\Users\\jzako\\source\\repos\\szoft_TT65SP\\Hajos teszt\\text.txt", true);//Local
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine() ?? string.Empty;
+                    string[] tomb = sor.Split('\t');
+                    if (tomb.Length != 7) continue;
 
-                Kerdes k = new Kerdes();
-                k.KerdesSzoveg = tomb[1];
-                k.Valasz1 = tomb[2];
-                k.Valasz2 = tomb[3];
-                k.Valasz3 = tomb[4];
-                k.URL = tomb[5];
+                    Kerdes k = new Kerdes();
+                    k.KerdesSzoveg = tomb[1];
+                    k.Valasz1 = tomb[2];
+                    k.Valasz2 = tomb[3];
+                    k.Valasz3 = tomb[4];
+                    k.URL = tomb[5];
 
-                int.TryParse(tomb[6], out int jovalasz);
+                    int.TryParse(tomb[6], out int jovalasz);
 
-                k.HelyesValasz = jovalasz;
+                    k.HelyesValasz = jovalasz;
 
-                lista.Add(k);
+                    lista.Add(k);
+                }
+                sr.Close();
+                return lista;
             }
-            sr.Close();
-            return lista;
-        }
+            catch {
+                MessageBox.Show("Hiba történt a beolvasás során");
+                return null;
+            }
+    }
     }
 }

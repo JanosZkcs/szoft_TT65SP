75de72b02d61b1fd479f456406e3c84386345d0c
Zakócs János <j.zakocs03@gmail.com>
2024-03-14 10:45:49 +0100

    Változás

--- Hajos teszt/Form1.cs
+++ Hajos teszt/Form1.cs
@@ -2,6 +2,9 @@ namespace Hajos_teszt
 {
     public partial class Form1 : Form
     {
+        List<Kerdes> OsszesKerdes;
+        List<Kerdes> AktivKerdesek;
+        int AktivKerdes = 5;
         public Form1()
         {
             InitializeComponent();
@@ -9,10 +12,33 @@ namespace Hajos_teszt
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            AktivKerdesek = new List<Kerdes>();
+            OsszesKerdes = KerdesBeolvasas();
         }
-    }
-    private void KerdesBeolvasas()
-    {
+        List<Kerdes> KerdesBeolvasas()
+        {
+            List<Kerdes> lista = new List<Kerdes>();
+            StreamReader sr = new StreamReader("text.txt",true);
+            while(!sr.EndOfStream) { 
+                string sor = sr.ReadLine() ?? string.Empty;
+                string[] tomb = sor.Split('\t');
+                if (tomb.Length != 7) continue;
+
+                Kerdes k = new Kerdes();
+                k.KerdesSzoveg = tomb[1];
+                k.Valasz1 = tomb[2];
+                k.Valasz2 = tomb[3];
+                k.Valasz3 = tomb[4];
+                k.URL = tomb[5];
+
+                int.TryParse(tomb[6], out int jovalasz);
+
+                k.HelyesValasz = jovalasz;
+
+                lista.Add(k);
+            }
+            sr.Close();
+            return lista;
+        }
     }
 }
